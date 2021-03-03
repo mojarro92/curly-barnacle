25fed3d5a6d0a28e07b966a3abbc64b32f48a6fc
Alejandro Mojarro <amojarrot@gmail.com>
2021-03-02 23:29:51 -0700

    navBarDraft

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import "bootswatch/dist/yeti/bootstrap.min.css";
 import './App.css';
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>My First Project</h1>
     </div>
   );
