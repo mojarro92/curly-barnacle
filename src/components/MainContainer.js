86b77e24ea1c9dfc55b2b81d25e82385f07ac056
Alejandro Mojarro <amojarrot@gmail.com>
2021-03-14 17:06:58 -0600

    gonnaDel

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import '../App.css';
+
 
 function MainContainer() {
-    return (
-        <div className="jumbotron">
 
+
+    return (
+        <div className="main_canvas d-flex justify-content-center align-items-center bg-secondary">
+            <canvas className="w-100 h-75 bg-warning"></canvas>
         </div>
     )
 }
